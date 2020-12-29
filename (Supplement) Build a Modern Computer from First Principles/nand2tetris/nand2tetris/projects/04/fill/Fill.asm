5880247df9d0cbab254e592b7394b596fe64396b
Fanchen Kong <Kong_fanchen@outlook.com>
2020-12-29 20:51:36 +0800

    update lab

--- (Supplement) Build a Modern Computer from First Principles/nand2tetris/nand2tetris/projects/04/fill/Fill.asm
+++ (Supplement) Build a Modern Computer from First Principles/nand2tetris/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(FOREVER) 
+@KBD
+D = M // read keyboard
+
+@WHITE
+D ; JEQ // goto white if keyboard is zero
+
+@BLACK
+D ; JNE // goto black if keyborad is typing
+
+
+(WHITE)
+@i
+M = 0 // set loop var to 0
+(WHITELOOP)
+@i 
+D = M // read the loop var D = i
+@8191
+D = D - A // D = i - 8191
+@FOREVER
+D ; JGT // if (i-8191) > 0 goto forever
+@i
+D = M // D = i
+@SCREEN
+A = A + D // A = A + i 
+M = 0 // RAM(16384+i) = 0x0000 set pixel to white
+@i
+M = M + 1 // i = i+1
+@WHITELOOP
+0 ; JMP   // goto loop
+
+(BLACK)
+@i
+M = 0 // set loop var to 0
+(BLACKLOOP)
+@i 
+D = M // read the loop var D = i
+@8191
+D = D - A // D = i - 8191
+@FOREVER
+D ; JGT // if (i-8191) > 0 goto forever
+@i
+D = M // D = i
+@SCREEN
+A = A + D // A = A + i 
+M = -1 // RAM(16384+i) = 0xFFFF set pixel to black
+@i
+M = M + 1 // i = i+1
+@BLACKLOOP
+0 ; JMP   // goto loop
--- (Supplement) Build a Modern Computer from First Principles/nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ (Supplement) Build a Modern Computer from First Principles/nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+@R2
+M = 0; // set sum = 0
+
+@R1
+D = M; 
+@i
+M = D; // load loop variable
+
+(LOOP)
+@i
+D = M // D = i
+@END
+D ; JLE // if i<=0 goto END
+@R0
+D = M // D = RAM[0]
+@R2
+M = M + D // sum = sum + 2
+@i
+M = M -1 // i = i - 1
+@LOOP
+0;JMP // goto loop  
+
+(END)
+0;JMP //infinite loop
